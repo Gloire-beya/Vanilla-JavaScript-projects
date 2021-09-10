fe8ebb102d25adf7bc376e5b6b7b2ad1ce4ee7bf
Gloire <gloweb78@gmail.com>
2021-09-10 10:28:49 +0200

    Countdown timer completed

---
+++ 12 countdown-timer/app.js
@@ -0,0 +1,87 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const weekdays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const giveaway = document.querySelector(".giveaway")
+const deadline = document.querySelector(".deadline")
+const items = document.querySelectorAll(".deadline-format h4")
+
+let tempDate = new Date()
+let tempYear = tempDate.getFullYear()
+let tempMonth = tempDate.getMonth()
+let tempDay = tempDate.getDate()
+
+//let futureDate = new Date(2021, 10, 9, 7, 45, 30)
+let futureDate = new Date(tempYear, tempMonth, tempDay + 10, 15, 30, 0)
+const year = futureDate.getFullYear()
+const hours = futureDate.getHours()
+const minutes = futureDate.getMinutes()
+
+let month = futureDate.getMonth()
+month = months[month]
+
+const date = futureDate.getDate()
+const weekDay = weekdays[futureDate.getDay()]
+
+const secondes = futureDate.getSeconds()
+
+giveaway.textContent = `giveaway ends on ${weekDay}, ${date} ${month} ${year} ${hours}:${minutes}`
+
+// future time in ms
+const futuretime = futureDate.getTime()
+
+function getRemainingTime() {
+  const today = new Date().getTime()
+  const t = futuretime - today
+
+  const oneDay = 24 * 60 * 60 * 1000
+  const oneHour = 60 * 60 * 1000
+  const oneMinute = 60 * 1000
+
+  // calculate all values
+  let days = Math.floor(t / oneDay)
+  let hours = Math.floor((t % oneDay) / oneHour)
+  let minutes = Math.floor((t % oneHour) / oneMinute)
+  let seconds = Math.floor((t % oneMinute) / 1000)
+
+  // set values array
+  const values = [days, hours, minutes, seconds]
+
+  function format(item) {
+    if (item < 10) return item = `0${item}`
+    return item
+  }
+
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index])
+  })
+  if (t < 0) {
+    clearInterval(countdown)
+    deadline.innerHTML = `<h4 class="expired"> sorry, this giveaway has expired</h4>`
+  }
+}
+
+// countdown
+
+let countdown = setInterval(getRemainingTime, 1000)
+getRemainingTime()
