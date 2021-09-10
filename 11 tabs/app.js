dd7ee28b258d7eb8444788f3bcc6dc931a755a39
Gloire <gloweb78@gmail.com>
2021-09-10 06:48:05 +0200

    Tabs project completed

---
+++ 11 tabs/app.js
@@ -0,0 +1,20 @@
+const about = document.querySelector(".about")
+const btns = document.querySelectorAll(".tab-btn")
+const articles = document.querySelectorAll(".content")
+
+about.addEventListener("click", (e) => {
+    const id = e.target.dataset.id
+    if (id) {
+        btns.forEach((btn) => {
+            btn.classList.remove("active")
+            e.target.classList.add("active")
+        })
+
+        // hide other articles
+        articles.forEach((article) => {
+            article.classList.remove("active")
+        })
+        const element = document.getElementById(id)
+        element.classList.add("active")
+    }
+})
